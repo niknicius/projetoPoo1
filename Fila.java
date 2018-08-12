b13d338ccf5094fefe46b9694ecad6b63a2eb60a
Amanda Pereira <amanda.souza@dce.ufpb.br>
2018-08-11 22:53:19 -0300

    Classes fila e exame

---
+++ Fila.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Fila {
+	private ArrayList<Paciente> pacientes;
+	
+	public Fila() {
+		this.pacientes = new ArrayList<>();
+		
+	}
+	
+	public ArrayList<Paciente> getPacientes(){
+		return this.pacientes;
+	}
+	
+	public void setPacientes(ArrayList<Paciente> pacientes) {
+		this.pacientes = pacientes;
+	}
+	
+	public void addPacienteNaFila(Paciente paciente) {
+		for(Paciente p: this.pacientes) {
+			if(p.getNome().equalsIgnoreCase(paciente.getNome())) {
+				this.pacientes.add(paciente);
+			}
+		}
+	}
+	
+	public void removerPacienteDaFila(Paciente paciente) {
+		for(Paciente p: this.pacientes) {
+			if(p.getNome().equalsIgnoreCase(paciente.getNome())) {
+				this.pacientes.remove(paciente);
+			}
+		}
+	}
+}
