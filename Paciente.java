0fab6b437d30114bf251f3353aaca728bf68bc9f
tulio700 <tulio.jose@dce.ufpb.br>
2018-08-12 00:35:39 -0300

    Classes Paciente e Prontuario

---
+++ Paciente.java
@@ -0,0 +1,127 @@
+package projetoPoo1;
+public class Paciente {
+	private String nome;
+	private String raca;
+	private String dataNascimento;
+	private String cpf;
+	private String rg;
+	private String sexo;
+	private String cns;
+	private String nacionalidade;
+	private String cep;
+	private String uf;
+
+
+	
+	public Paciente (){
+	}
+	
+	public Paciente (String nome, String raca, String dataNascimento, String cpf, String rg, String sexo, String cns, String nacionalidade, String cep, String uf) {
+	this.nome = nome;
+	this.raca = raca;
+	this.dataNascimento = dataNascimento;
+	this.cpf = cpf;
+	this.rg = rg;
+	this.sexo = sexo;
+	this.cns = cns;
+	this.nacionalidade = nacionalidade;
+	this.cep = cep;
+	this.uf = uf;
+	}
+	
+	public Paciente (String nome, String raca, String dataNascimento, String cpf, String rg, String sexo, String nacionalidade, String cep, String uf) {
+	this.nome = nome;
+	this.raca = raca;
+	this.dataNascimento = dataNascimento;
+	this.cpf = cpf;
+	this.rg = rg;
+	this.sexo = sexo;
+	this.nacionalidade = nacionalidade;
+	this.cep = cep;
+	this.uf = uf;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String dataNascimento() {
+		return this.dataNascimento;
+	}
+	
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public String getCpf() {
+		return this.cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCns() {
+		return cns;
+	}
+
+	public void setCns(String cns) {
+		this.cns = cns;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+}
---
+++ Prontuario.java
@@ -0,0 +1,82 @@
+package projetoPoo1;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Prontuario {
+	
+	private Paciente paciente;
+	private String dataProntuario;
+	private List<Exame> listaExame;
+	private String diagnostico;
+	private Medico medicoAtual;
+	private List<String> sintomas;
+	
+	public Prontuario (Paciente paciente, String dataProntuario, ArrayList<Exame> exame, String diagnostico, Medico medicoAtual, ArrayList<String> sintomas) {
+		this.paciente = paciente;
+		this.dataProntuario = dataProntuario;
+		this.listaExame = exame;
+		this.diagnostico = diagnostico;
+		this.medicoAtual = medicoAtual;
+		this.sintomas = sintomas;
+		}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public String getDataProntuario() {
+		return dataProntuario;
+	}
+
+	public void setDataProntuario(String dataProntuario) {
+		this.dataProntuario = dataProntuario;
+	}
+
+	public List<Exame> getExame() {
+		return this.listaExame;
+	}
+
+	public void setExame(ArrayList<Exame> exame) {
+		this.listaExame = exame;
+	}
+
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}
+
+	public Medico getMedicoAtual() {
+		return medicoAtual;
+	}
+
+	public void setMedicoAtual(Medico medicoAtual) {
+		this.medicoAtual = medicoAtual;
+	}
+
+	public List<String> getSintomas() {
+		return sintomas;
+	}
+
+	public void setSintomas(ArrayList<String> sintomas) {
+		this.sintomas = sintomas;
+	}
+	
+	public void novoSintoma(String sintomaNovo) {
+		this.sintomas.add(sintomaNovo);
+	}
+	
+	public void novoDiagnostico(String diagnosticoNovo) {
+		this.diagnostico = diagnosticoNovo;
+	}
+	
+	public void novoExame(Exame exameNovo) {
+		this.listaExame.add(exameNovo);
+	}
+}
